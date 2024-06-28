818247a96972c4ad44f310faf2a9ba2b60f31109
hyeonjun.lee <little6523@naver.com>
2024-06-28 16:45:25 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-28 강의 시간: 15:00 ~ 16:00 강의 내용 - Bean - Singleton

---
+++ src/main/java/com/ormispring/basic/Singleton.java
@@ -0,0 +1,15 @@
+package com.ormispring.basic;
+
+public class Singleton {
+
+  private static final Singleton instance = new Singleton();
+
+  private Singleton() {
+
+  }
+
+  public static Singleton getInstance() {
+    return instance;
+  }
+
+}
