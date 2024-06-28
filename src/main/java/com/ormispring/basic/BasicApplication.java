983876627cb0502644f4af010fb71c7da911ccd6
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2024-06-28 16:46:18 +0900

    Update BasicApplication.java
    [ESTsoft 오르미 5기 백엔드]
    강의 날짜: 2024-06-28
    강의 시간: 15:00 ~ 16:00
    강의 내용
    - Bean
    - Singleton

--- src/main/java/com/ormispring/basic/BasicApplication.java
+++ src/main/java/com/ormispring/basic/BasicApplication.java
@@ -7,6 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(BasicApplication.class, args);
+
+    Singleton singleton = Singleton.getInstance();
+    Singleton singleton1 = Singleton.getInstance();
+    Singleton singleton2 = Singleton.getInstance();
+
+    System.out.println(singleton);
+    System.out.println("====================================");
+    System.out.println(singleton1);
+    System.out.println("====================================");
+    System.out.println(singleton2);
+    System.out.println("====================================");
+
+//        SpringApplication.run(BasicApplication.class, args);
   }
 }
