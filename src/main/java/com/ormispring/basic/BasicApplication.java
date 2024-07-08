2cf228f3356f14b05350694dd5696d2328c7403f
hyeonjun.lee <little6523@naver.com>
2024-07-08 15:52:48 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-07-08 강의 시간: 15:00 ~ 16:00 강의 내용 - dispatcherServlet

--- src/main/java/com/ormispring/basic/BasicApplication.java
+++ src/main/java/com/ormispring/basic/BasicApplication.java
@@ -1,16 +1,29 @@
 package com.ormispring.basic;
 
 import com.ormispring.service.MyService;
+import jakarta.servlet.ServletException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.IOException;
 
 @SpringBootApplication(scanBasePackages = "com.ormispring")
 public class BasicApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ServletException, IOException {
 
-//    MyService myService = new MyService();
-//    myService.hello();
-    SpringApplication.run(BasicApplication.class, args);
+    DispatcherServlet dispatcherservlet = new DispatcherServlet();
+    String url = "/hello";
+
+    MockHttpServletRequest request = new MockHttpServletRequest(url);
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    dispatcherservlet.service(request, response);
+
+    System.out.println(response.getContent());
+    System.out.println("응답 내용" + response.getContent());
+
+    //    SpringApplication.run(BasicApplication.class, args);
   }
 }
