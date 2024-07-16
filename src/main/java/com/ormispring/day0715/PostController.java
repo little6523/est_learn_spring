6b9a6ea2d2e527ffa00a65d8fa3836dc9ec7dbef
hyeonjun.lee <little6523@naver.com>
2024-07-16 13:48:45 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-07-16 강의 시간: 13:00 ~ 14:00 강의 내용 - 게시판 구현 update

--- src/main/java/com/ormispring/day0715/PostController.java
+++ src/main/java/com/ormispring/day0715/PostController.java
@@ -12,43 +12,68 @@ import java.util.List;
 @RequestMapping("/posts")
 public class PostController {
 
-    private List<Post> posts = new ArrayList<>();
-    private Long nextId = 1L;
+  private List<Post> posts = new ArrayList<>();
+  private Long nextId = 1L;
 
-    @GetMapping
-    public String list(Model model) {
-        model.addAttribute("posts", posts);
-        return "post/list";
-    }
+  @GetMapping
+  public String list(Model model) {
+    model.addAttribute("posts", posts);
+    return "post/list";
+  }
 
-    @GetMapping("/new")
-    public String newPostForm(Model model) {
-        model.addAttribute("post", new Post());
-        return "post/form";
-    }
+  @GetMapping("/new")
+  public String newPostForm(Model model) {
+    model.addAttribute("post", new Post());
+    return "post/form";
+  }
 
-    @PostMapping
-    public String savePost(@ModelAttribute Post post) {
-        post.setId(nextId++);
-        post.setCreateAt(LocalDateTime.now());
-        posts.add(post);
-        return "redirect:/posts";
-    }
+  @PostMapping
+  public String savePost(@ModelAttribute Post post) {
+    post.setId(nextId++);
+    post.setCreateAt(LocalDateTime.now());
+    posts.add(post);
+    return "redirect:/posts";
+  }
 
-    @GetMapping("/{id}")
-    public String detail(@PathVariable("id") Long id, Model model) {
-        Post post = posts.stream()
-                .filter(p -> p.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException());
+  @GetMapping("/{id}")
+  public String detail(@PathVariable("id") Long id, Model model) {
+    Post post =
+        posts.stream()
+            .filter(p -> p.getId() == id)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException());
 
-        model.addAttribute("post", post);
-        return "post/detail";
-    }
+    model.addAttribute("post", post);
+    return "post/detail";
+  }
 
-    @PostMapping("/{id}/delete")
-    public String delete(@PathVariable("id") Long id) {
-        posts.removeIf(post -> post.getId()==id);
-        return "redirect:/posts";
-    }
+  @PostMapping("/{id}/delete")
+  public String delete(@PathVariable("id") Long id) {
+    posts.removeIf(post -> post.getId() == id);
+    return "redirect:/posts";
+  }
+
+  @GetMapping("{id}/edit")
+  public String editForm(@PathVariable("id") Long id, Model model) {
+    Post post =
+        posts.stream()
+            .filter(p -> p.getId() == id)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException());
+
+    model.addAttribute("post", post);
+    return "post/edit";
+  }
+
+  @PostMapping("/{id}/edit")
+  public String edit(@PathVariable("id") Long id, @ModelAttribute Post updatePost) {
+      Post post = posts.stream()
+              .filter(p -> p.getId() == id)
+              .findFirst()
+              .orElseThrow(() -> new IllegalArgumentException());
+
+      post.setTitle(updatePost.getTitle());
+      post.setContent(updatePost.getContent());
+      return "redirect:/posts/{id}";
+  }
 }
